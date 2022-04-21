b508a4775754784357be7bde4fe8130a0a75201d
nmanren <narendrababulinux@gmail.com>
2022-04-21 07:07:19 +0000

    added files

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+terraform {
+    required_version = "> 0.12.0"
+# The below section if not specified the terraform.tfstate file is created in current directory of your machine.
+ backend "azurerm" {
+ 
+  }
+}
+
